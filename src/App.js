97f68a6d3b16a3b6b83a63437000ce21c7523127
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-02-24 03:29:23 +0500

    fix linters

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TodoList from './components/TodoList';
 function App() {
   return (
     <>
-      <TodoList/>
+      <TodoList />
     </>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,78 +1,85 @@
-import React, { useEffect, useState } from 'react'
-import binImage from '../assets/icons/bin.png'
+import React, { useEffect, useState } from 'react';
+import binImage from '../assets/icons/bin.png';
 
 function TodoList() {
+  const [inpuValue, setInputValue] = useState('');
+  const [array, setArray] = useState([]);
 
-  const [inpuValue, setInputValue] = useState('')
-  const [array, setArray] = useState([])
-  
-  function addTodo () {
-    if(inpuValue){
-      let object = {
+  function addTodo() {
+    if (inpuValue) {
+      const object = {
         value: inpuValue,
-        completed: false
-      }
-      setArray([...array, object])
-      localStorage.setItem('todo-list', JSON.stringify([...array, object]))
+        completed: false,
+      };
+      setArray([...array, object]);
+      localStorage.setItem('todo-list', JSON.stringify([...array, object]));
     }
   }
 
-  useEffect(()=>{
-    let getItem = JSON.parse(localStorage.getItem('todo-list'))
-    if(getItem !== null){
-      setArray(getItem)
+  useEffect(() => {
+    const getItem = JSON.parse(localStorage.getItem('todo-list'));
+    if (getItem !== null) {
+      setArray(getItem);
     }
-  },[])
+  }, []);
 
   function ischecked(e) {
-    console.log(e.target.checked);
-  let newArray = []
-  for (let i = 0; i < array.length; i++) {
-    if (i === parseInt(e.target.parentElement.classList[0])){
-      newArray.push(array[i])
-      if (!newArray[i].completed){
-        newArray[i].completed = true
-        e.target.parentElement.style.opacity = '50%'
-      }else{
-        newArray[i].completed = false
-        e.target.parentElement.style.opacity = '100%'
+    const newArray = [];
+    for (let i = 0; i < array.length; i += 1) {
+      if (i === parseInt(e.target.parentElement.classList[0], 10)) {
+        newArray.push(array[i]);
+        if (!newArray[i].completed) {
+          newArray[i].completed = true;
+        } else {
+          newArray[i].completed = false;
+        }
+      } else {
+        newArray.push(array[i]);
       }
-    }else{
-      newArray.push(array[i])
     }
+    setArray(newArray);
+    localStorage.setItem('todo-list', JSON.stringify(newArray));
   }
-    setArray(newArray)
-    localStorage.setItem('todo-list', JSON.stringify(newArray))
-  }
-  
+
   function delet(e) {
-  let newArray = []
-  for (let i = 0; i < array.length; i++) {
-    if (i !== parseInt(e.target.parentElement.classList[0])){
-      newArray.push(array[i])
+    const newArray = [];
+    for (let i = 0; i < array.length; i += 1) {
+      if (i !== parseInt(e.target.parentElement.classList[0], 10)) {
+        newArray.push(array[i]);
+      }
     }
-  }
-  setArray(newArray);
-  localStorage.setItem('todo-list', JSON.stringify(newArray))
+    setArray(newArray);
+    localStorage.setItem('todo-list', JSON.stringify(newArray));
   }
 
   return (
     <>
-    <div className="">
-      <input type="text" className="todo-input" onChange={(e)=>{setInputValue(e.target.value)}}/>
-      <button className="add-todo-btn" onClick={addTodo}>+</button>
-    </div>
-    <ul className="">
-      {(array)? array.map((e, index) => (
-      <li key={index} className={index}>
-          <input type="checkbox" checked={e.completed} onChange={ischecked}/>
-        {e.value}
-          <img src={binImage} alt="bin" onClick={delet} />
-      </li>
-      )): ''}
-    </ul>
+      <div className="">
+        <input type="text" className="todo-input" onChange={(e) => { setInputValue(e.target.value); }} />
+        <button type="button" className="add-todo-btn" onClick={addTodo}>+</button>
+      </div>
+      <ul className="">
+        {(array) ? array.map((e, index) => (
+          // eslint-disable-next-line
+          <li key={`li-${index}`} className={index} style={{ opacity: (e.completed ? '50%' : '100%') }}>
+            <input type="checkbox" checked={e.completed} onChange={ischecked} />
+            {e.value}
+            {/* eslint-disable-next-line */}
+            <img
+              src={binImage}
+              alt="bin"
+              onClick={delet}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter' || event.key === ' ') {
+                  delet(event);
+                }
+              }}
+            />
+          </li>
+        )) : ''}
+      </ul>
     </>
-  )
+  );
 }
 
 export default TodoList;
